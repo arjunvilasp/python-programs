d169e750a88b468f5826fe0d5c755d0c770d6f82
Arjun Vilas P <108401864+arjunvilasp@users.noreply.github.com>
2022-10-21 11:26:33 +0530

    Add files via upload

---
+++ swapWithoutTemp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+a=10
+b=20
+print("before swapping value of a, b=",a,b)
+a = a + b
+b = a - b
+a = a - b
+print("before swapping value of a, b=",a,b)
+
+
+# In[ ]:
+
+
+
+
