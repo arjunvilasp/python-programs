807b7e1940988f776f7f42c538f3bf8472c4684b
Arjun Vilas P <108401864+arjunvilasp@users.noreply.github.com>
2022-10-28 10:49:22 +0530

    Add files via upload

---
+++ areaOfTriangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+s=(a+b+c)/2
+area=(s*(s-a)*(s-b)-(s-c))**.5
+print("Area = ",area)
+
+
+# In[ ]:
+
+
+
+
