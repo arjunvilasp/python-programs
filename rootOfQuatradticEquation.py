f0d487b4368b94a81bf3dc157fb8cd8411e3aea3
Arjun Vilas P <108401864+arjunvilasp@users.noreply.github.com>
2022-10-28 10:39:06 +0530

    Add files via upload

---
+++ rootOfQuatradticEquation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import cmath
+
+a=4
+b=6
+c=2
+d=(b**2)-(4*a*c)
+sol1=(-b+cmath.sqrt(d))/(2*a)
+sol2=(-b-cmath.sqrt(d))/(2*a)
+print("sqrt of quadratic equation is {} and {}".format(sol1,sol2))
+
+
+# In[ ]:
+
+
+
+
